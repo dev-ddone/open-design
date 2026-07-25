defa0b61af20654d701ebf126083c538ff74468f
clapts <186709728+clapts@users.noreply.github.com>
2026-07-25 19:57:11 +0200

    feat(editor): mount contextual toolbar and crop workflow

--- src/client/components/editor.tsx
+++ src/client/components/editor.tsx
@@ -1,5 +1,7 @@
 import { CanvasArea } from "./canvas-area";
 import { Toolbar } from "./toolbar";
+import { SelectionToolbar } from "./selection-toolbar";
+import { CropPanel } from "./crop-panel";
 import { LeftSidebar } from "./left-sidebar";
 import { RightSidebar } from "./right-sidebar";
 import { PagesBar } from "./pages-bar";
@@ -9,8 +11,9 @@ export function Editor() {
   const { readOnly } = useEditor();
 
   return (
-    <div class="flex flex-col h-full w-full">
+    <div class="relative flex h-full w-full flex-col">
       <Toolbar />
+      <SelectionToolbar />
       <div class="flex flex-1 min-h-0">
         <LeftSidebar />
         <div class="flex-1 flex flex-col min-w-0">
@@ -19,6 +22,7 @@ export function Editor() {
         </div>
         {!readOnly && <RightSidebar />}
       </div>
+      <CropPanel />
     </div>
   );
 }
