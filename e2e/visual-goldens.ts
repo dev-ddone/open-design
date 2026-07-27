e9a4cddae10b1629fc1fa0a66f28441f482049f3
clapts <186709728+clapts@users.noreply.github.com>
2026-07-28 01:45:11 +0200

    test: accept reviewed non-overlapping editor layout

--- e2e/visual-goldens.ts
+++ e2e/visual-goldens.ts
@@ -1,6 +1,6 @@
 const visualGoldens = {
-  editorEmptyChromiumLinux: "77336dc9677f54425665015dd82f649b5410d5835f319c6d4d38f9b10235b8eb",
-  assetPickerChromiumLinux: "8dcbd03f6ecb8c833e8e2d6c08b40399b36a76f5b2cbf1147b96693dde3584ee",
+  editorEmptyChromiumLinux: "52aacdaf4a3a911c167a66014838ce419a99e4e8dba8aac4b9ccf810446c495e",
+  assetPickerChromiumLinux: "ddb2ccabf1418fc78403587319144c2bc0f1bd4ba7c7530282c20738524e5864",
 } as const;
 
 export default visualGoldens;
