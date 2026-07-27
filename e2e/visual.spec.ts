7dc74cb41beccabb4b62e1c1ece34dab0ffb4963
clapts <186709728+clapts@users.noreply.github.com>
2026-07-28 00:14:17 +0200

    test: load visual golden hashes from TypeScript

--- e2e/visual.spec.ts
+++ e2e/visual.spec.ts
@@ -1,6 +1,6 @@
 import { createHash } from "node:crypto";
 import { expect, test } from "@playwright/test";
-import visualGoldens from "./visual-goldens.json";
+import visualGoldens from "./visual-goldens";
 import { openCommand, openFreshDesign } from "./helpers";
 
 function screenshotHash(buffer: Buffer): string {
